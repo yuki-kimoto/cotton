623c31bc070938aae71785ccdf9e7f1d118dc061
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-03-15 15:39:57 +0900

    ボタンの背景を作成

--- lib/Cotton.c
+++ lib/Cotton.c
@@ -2,31 +2,60 @@
 
 #include <windows.h>
 
-int WINAPI WinMain(
-    HINSTANCE hInstance ,
-    HINSTANCE hPrevInstance ,
-    PSTR lpCmdLine ,
-    int nCmdShow ) {
-  HWND hwnd;
-  MSG msg;
+LRESULT CALLBACK WndProc(HWND hwnd , UINT msg , WPARAM wp , LPARAM lp) {
+	HDC hdc;
+	HPEN hpen;
+	PAINTSTRUCT ps;
 
-  hwnd = CreateWindow(
-      TEXT("BUTTON") , TEXT("Kitty on your lap") ,
-      WS_CAPTION | WS_VISIBLE ,
-      100 , 100 , 200 , 200 , NULL , NULL ,
-      hInstance , NULL
-  );
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd , &ps);
+		hpen = CreatePen(PS_SOLID , 0 , 0x00AA77);
+		SelectObject(hdc, hpen);
+    SelectObject(hdc, CreateSolidBrush(0x00AA77));
+		Rectangle(hdc , 10 , 10 , 200 , 50);
 
-  if (hwnd == NULL) return 0;
-
-  while (TRUE) {
-    GetMessage(&msg , NULL , 0 , 0);
-    if (msg.message == WM_LBUTTONUP) break;
-    DispatchMessage(&msg);
-  }
-  return 0;
+		EndPaint(hwnd , &ps);
+		DeleteObject(hpen);
+		return 0;
+	}
+	return DefWindowProc(hwnd , msg , wp , lp);
 }
 
+int WINAPI WinMain(HINSTANCE hInstance , HINSTANCE hPrevInstance ,
+			PSTR lpCmdLine , int nCmdShow ) {
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS winc;
+
+	winc.style		= CS_HREDRAW | CS_VREDRAW;
+	winc.lpfnWndProc	= WndProc;
+	winc.cbClsExtra	= winc.cbWndExtra	= 0;
+	winc.hInstance		= hInstance;
+	winc.hIcon		= LoadIcon(NULL , IDI_APPLICATION);
+	winc.hCursor		= LoadCursor(NULL , IDC_ARROW);
+	winc.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
+	winc.lpszMenuName	= NULL;
+	winc.lpszClassName	= TEXT("KITTY");
+
+	if (!RegisterClass(&winc)) return -1;
+
+	hwnd = CreateWindow(
+			TEXT("KITTY") , TEXT("Kitty on your lap") ,
+			WS_OVERLAPPEDWINDOW | WS_VISIBLE ,
+			CW_USEDEFAULT , CW_USEDEFAULT ,
+			CW_USEDEFAULT , CW_USEDEFAULT ,
+			NULL , NULL , hInstance , NULL
+	);
+
+	if (hwnd == NULL) return -1;
+
+	while(GetMessage(&msg , NULL , 0 , 0)) DispatchMessage(&msg);
+	return msg.wParam;
+}
 int32_t SPNATIVE__Cotton__call_win_main(SPVM_ENV* env, SPVM_VALUE* args) {
   (void)env;
   (void)args;
