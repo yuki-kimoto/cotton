ad1b0b995fb57ad8c76e310e1da795d308da41b9
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-05-26 16:55:45 +0900

    Check errors

--- lib/SPVM/Cotton/Runtime/Engine/Win.cpp
+++ lib/SPVM/Cotton/Runtime/Engine/Win.cpp
@@ -625,16 +625,17 @@ int32_t SPVM__Cotton__Runtime__Engine__Win__create_main_window(SPVM_ENV* env, SP
       
       // Create vertex buffer
       hr = g_pd3dDevice->CreateBuffer( &bufferDesc, &InitData, &g_pVertexBuffer );
+
+      if (FAILED(hr)) {
+        return hr;
+      }
       
       // Shape of vertex
       D3D11_INPUT_ELEMENT_DESC g_VertexDesc[] {
         { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0,                            0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
         { "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
       };
-      
-      if (FAILED(hr)) {
-        return hr;
-      }
+
     }
   }
   return 0;
