51e49cb031f731c2a94d29108cbb22e266891b85
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-08 10:50:02 +0900

    Cleanup.

--- lib/SPVM/Engine/Runtime/Windows/API.cpp
+++ lib/SPVM/Engine/Runtime/Windows/API.cpp
@@ -139,7 +139,7 @@ static int32_t repaint(SPVM_ENV* env, SPVM_VALUE* stack, void* obj_self) {
       stack[0].oval = obj_renderer;
       stack[1].oval = obj_runtime;
       stack[2].oval = obj_paint_info;
-      env->call_instance_method_by_name(env, stack, "repaints", 2, &error_id, __func__, FILE_NAME, __LINE__);
+      env->call_instance_method_by_name(env, stack, "repaint", 2, &error_id, __func__, FILE_NAME, __LINE__);
       if (error_id) { return error_id; }
       
       free(paint_info);
