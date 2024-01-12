8686902c149eb2466c7deae394732765ff7b1f8d
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-12 13:09:25 +0900

    Add Windows size tests.

--- lib/SPVM/Eg/OS/Windows/API/Window.cpp
+++ lib/SPVM/Eg/OS/Windows/API/Window.cpp
@@ -57,4 +57,49 @@ int32_t SPVM__Eg__OS__Windows__API__Window__inner_height(SPVM_ENV* env, SPVM_VAL
   return 0;
 }
 
+int32_t SPVM__Eg__OS__Windows__API__Window__outer_width(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  stack[0].oval = obj_self;
+  stack[1].oval = env->new_string_nolen(env, stack, "window_handle");
+  env->call_instance_method_by_name(env, stack, "get_data", 2, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  void* obj_window_handle = stack[0].oval;
+  
+  HWND window_handle = (HWND)env->get_pointer(env, stack, obj_window_handle);
+  
+  RECT rect;
+  GetWindowRect(window_handle, &rect);
+  
+  stack[0].ival = rect.right + 1;
+  
+  return 0;
+}
+
+
+int32_t SPVM__Eg__OS__Windows__API__Window__outer_height(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  stack[0].oval = obj_self;
+  stack[1].oval = env->new_string_nolen(env, stack, "window_handle");
+  env->call_instance_method_by_name(env, stack, "get_data", 2, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  void* obj_window_handle = stack[0].oval;
+  
+  HWND window_handle = (HWND)env->get_pointer(env, stack, obj_window_handle);
+  
+  RECT rect;
+  GetWindowRect(window_handle, &rect);
+  
+  stack[0].ival = rect.bottom + 1;
+  
+  return 0;
+}
+
 }
