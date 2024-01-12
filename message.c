14cf1b51ea1785582c9aae830e1d2d3b0d17299c
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-12 10:41:24 +0900

    Remove unneeded files.

--- message.c
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int showCmd)
-{
-    MessageBox(NULL, "Hello world.", "メッセージ", MB_OK);
-
-    return 0;
-}
