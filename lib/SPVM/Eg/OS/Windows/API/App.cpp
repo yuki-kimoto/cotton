4e73a13bd1d4989d944951f3ca0ca6b86ebda162
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-12 12:59:57 +0900

    Remove public inner_width, inner_height from App.

--- lib/SPVM/Eg/OS/Windows/API/App.cpp
+++ lib/SPVM/Eg/OS/Windows/API/App.cpp
@@ -805,49 +805,4 @@ int32_t SPVM__Eg__OS__Windows__API__App__paint_node(SPVM_ENV* env, SPVM_VALUE* s
   return 0;
 }
 
-int32_t SPVM__Eg__OS__Windows__API__App__inner_width(SPVM_ENV* env, SPVM_VALUE* stack) {
-  
-  int32_t error_id = 0;
-  
-  void* obj_self = stack[0].oval;
-  
-  stack[0].oval = obj_self;
-  stack[1].oval = env->new_string_nolen(env, stack, "window_handle");
-  env->call_instance_method_by_name(env, stack, "get_data", 2, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  void* obj_window_handle = stack[0].oval;
-  
-  HWND window_handle = (HWND)env->get_pointer(env, stack, obj_window_handle);
-  
-  RECT rect;
-  GetClientRect(window_handle, &rect);
-  
-  stack[0].ival = rect.right + 1;
-  
-  return 0;
-}
-
-
-int32_t SPVM__Eg__OS__Windows__API__App__inner_height(SPVM_ENV* env, SPVM_VALUE* stack) {
-  
-  int32_t error_id = 0;
-  
-  void* obj_self = stack[0].oval;
-  
-  stack[0].oval = obj_self;
-  stack[1].oval = env->new_string_nolen(env, stack, "window_handle");
-  env->call_instance_method_by_name(env, stack, "get_data", 2, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  void* obj_window_handle = stack[0].oval;
-  
-  HWND window_handle = (HWND)env->get_pointer(env, stack, obj_window_handle);
-  
-  RECT rect;
-  GetClientRect(window_handle, &rect);
-  
-  stack[0].ival = rect.bottom + 1;
-  
-  return 0;
-}
-
 }
