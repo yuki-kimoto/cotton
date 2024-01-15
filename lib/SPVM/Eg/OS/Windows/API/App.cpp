832b54490e519cd4a57a99234d52185d07623559
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-15 10:51:58 +0900

    separate struct spvm__eg__layout__box.

--- lib/SPVM/Eg/OS/Windows/API/App.cpp
+++ lib/SPVM/Eg/OS/Windows/API/App.cpp
@@ -12,6 +12,8 @@
 
 #include "re2/re2.h"
 
+#include "spvm__eg__layout__box.h"
+
 static const char* FILE_NAME = "Eg/OS/Windows/API/App.cpp";
 
 extern "C" {
@@ -264,27 +266,6 @@ static void alert(SPVM_ENV* env, SPVM_VALUE* stack, const char* message) {
 struct COTTON_RUNTIME_PAINT_INFO {
   HDC hdc;
   ID2D1HwndRenderTarget* renderer;
-};
-
-struct spvm__eg__layout__box {
-  float color_red;
-  float color_green;
-  float color_blue;
-  float color_alpha;
-  int32_t has_background_color;
-  float background_color_red;
-  float background_color_green;
-  float background_color_blue;
-  float background_color_alpha;
-  int32_t left;
-  int32_t top;
-  int32_t width;
-  int32_t height;
-  struct spvm__eg__layout__box* first;
-  struct spvm__eg__layout__box* last;
-  struct spvm__eg__layout__box* sibparent;
-  int8_t moresib;
-  const char* text;
 };
 
 static int32_t paint_event_handler(SPVM_ENV* env, SPVM_VALUE* stack, void* obj_self) {
