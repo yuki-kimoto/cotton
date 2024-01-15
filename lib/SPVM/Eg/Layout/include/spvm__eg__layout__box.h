db52781362cc3f255d5ec188281ff29160a3c80b
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-15 10:22:51 +0900

    Add Layout::Box resource.

---
+++ lib/SPVM/Eg/Layout/include/spvm__eg__layout__box.h
@@ -0,0 +1,22 @@
+#include <spvm_native.h>
+
+struct spvm__eg__layout__box {
+  float color_red;
+  float color_green;
+  float color_blue;
+  float color_alpha;
+  int32_t has_background_color;
+  float background_color_red;
+  float background_color_green;
+  float background_color_blue;
+  float background_color_alpha;
+  int32_t left;
+  int32_t top;
+  int32_t width;
+  int32_t height;
+  struct spvm__eg__layout__box* first;
+  struct spvm__eg__layout__box* last;
+  struct spvm__eg__layout__box* sibparent;
+  int8_t moresib;
+  const char* text;
+};
