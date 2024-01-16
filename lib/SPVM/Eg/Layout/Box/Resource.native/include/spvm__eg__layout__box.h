38d80e3f2726a37182896d45a64d8f4fff53e676
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-16 09:09:12 +0900

    Add is_anon_box.

--- lib/SPVM/Eg/Layout/Box/Resource.native/include/spvm__eg__layout__box.h
+++ lib/SPVM/Eg/Layout/Box/Resource.native/include/spvm__eg__layout__box.h
@@ -6,23 +6,24 @@ enum {
 }
 
 struct spvm__eg__layout__box {
-  float color_red;
-  float color_green;
-  float color_blue;
-  float color_alpha;
-  int32_t has_background_color;
-  float background_color_red;
-  float background_color_green;
-  float background_color_blue;
-  float background_color_alpha;
-  int32_t left;
-  int32_t top;
-  int32_t width;
-  int32_t height;
   struct spvm__eg__layout__box* first_child;
   struct spvm__eg__layout__box* last_child;
   struct spvm__eg__layout__box* next_sibling;
   struct spvm__eg__layout__box* parent_node;
   const char* text;
+  int32_t left;
+  int32_t top;
+  int32_t width;
+  int32_t height;
+  int32_t has_background_color;
+  float color_red;
+  float color_green;
+  float color_blue;
+  float color_alpha;
+  float background_color_red;
+  float background_color_green;
+  float background_color_blue;
+  float background_color_alpha;
   int8_t box_sizing;
+  int8_t is_anon_box;
 };
